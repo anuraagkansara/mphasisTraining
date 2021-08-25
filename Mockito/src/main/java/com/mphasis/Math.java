259a76d34e88705214973837ed870bb430095000
anuraagkansara <anuraagkansara@gmail.com>
2021-08-25 17:23:39 +0530

    Hibernate

--- Mockito/src/main/java/com/mphasis/Math.java
+++ Mockito/src/main/java/com/mphasis/Math.java
@@ -2,24 +2,22 @@ package com.mphasis;
 
 import com.mphasis.services.CalculatorService;
 
-public class Math implements CalculatorService{
-    @Override
-    public double add(double firstNumber, double secondNumber) {
-        return 0;
+public class Math {
+    private CalculatorService calculatorService;
+
+    public double add(double firstNumber, double secondNumber){
+        return calculatorService.add(firstNumber, secondNumber);
+    }
+    public double subtract(double firstNumber, double secondNumber){
+        return calculatorService.subtract(firstNumber, secondNumber);
+    }
+    public double multiply(double firstNumber, double secondNumber){
+        return calculatorService.multiply(firstNumber, secondNumber);
+    }
+    public double divide(double firstNumber, double secondNumber){
+        return calculatorService.divide(firstNumber, secondNumber);
     }
 
-    @Override
-    public double subtract(double firstNumber, double secondNumber) {
-        return 0;
-    }
 
-    @Override
-    public double multiply(double firstNumber, double secondNumber) {
-        return 0;
-    }
 
-    @Override
-    public double divide(double firstNumber, double secondNumber) {
-        return 0;
-    }
 }
--- Mockito/src/test/java/tests/Main.java
+++ Mockito/src/test/java/tests/Main.java
@@ -5,6 +5,7 @@ import com.mphasis.Portfolio;
 import com.mphasis.entities.Stock;
 import com.mphasis.services.CalculatorService;
 import com.mphasis.services.StockService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -15,8 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class Main {
@@ -31,14 +31,13 @@ public class Main {
     CalculatorService calculatorService;
 
 
-
 //    public static void main(String[] args){
 //        Main main = new Main();
 //        main.setUp();
 //        System.out.println(main.testMarketValue());
 //    }
 
-//    public void setUp(){
+    //    public void setUp(){
 //        portfolio = new Portfolio();
 //
 ////        stockService = mock(StockService.class);
@@ -46,10 +45,10 @@ public class Main {
 //        portfolio.setStockService(stockService);
 //    }
     @Test
-    public void testMarketValue(){
+    public void testMarketValue() {
         List<Stock> stocks = new ArrayList<Stock>();
-        Stock googleStock = new Stock("1","Google",10);
-        Stock microsoftStock = new Stock("2","Microsoft",10);
+        Stock googleStock = new Stock("1", "Google", 10);
+        Stock microsoftStock = new Stock("2", "Microsoft", 10);
 
         stocks.add(googleStock);
         stocks.add(microsoftStock);
@@ -59,14 +58,27 @@ public class Main {
         when(stockService.getPrice(googleStock)).thenReturn(50.00);   //Mocking the method,
         when(stockService.getPrice(microsoftStock)).thenReturn(1000.00);
 
-        when(calculatorService.add(1,2)).thenReturn(3.0);
-        double addResult = maths.add(1,2);
-
-        assertEquals(3.0,addResult,1);
-
         double marketValue = portfolio.getMarketValue();
 //        return marketValue == 10500.0;
-        assertEquals(10500.0,marketValue,1);
+        assertEquals(10500.0, marketValue, 1);
+
+        when(calculatorService.add(1.0, 2.0)).thenReturn(3.0);
+        double addResult = maths.add(1.0, 2.0);
+
+        assertEquals(3.0, addResult, 1);
+
+
     }
 
+    @Test
+    public void testAdd() {
+        //add the behavior to throw exception
+        doThrow(new RuntimeException("Add operation not implemented"))
+                .when(calculatorService).add(10.0, 20.0);
+
+
+//        test the add functionality
+        Assert.assertEquals(maths.add(10.0, 20.0), 30.0, 0);
+    }
 }
+
